4c904a44b88a015c2bba18c622755e2a9819f9fd
Vishnu.C P <115421837+vishnu-13-97@users.noreply.github.com>
2025-03-21 14:57:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
-const express = require ("express");
+const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./db");
 
-const menuRoutes = require("./router/menuRouter")
-const app = express();
-app.use(express.json());
-app.use(cors( {origin: ["http://localhost:5173", "https://deepnetassignment-1.onrender.com/"], 
-    methods: "GET,POST,PUT,DELETE"}));
+const menuRoutes = require("./router/menuRouter");
 
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// CORS configuration
+app.use(cors({
+  origin: ["http://localhost:5173", "https://deepnetassignment-1.onrender.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+// Connect to the database
 connectDB();
 
-
+// Routes
 app.use("/api", menuRoutes);
 
+// Set up server to listen on a specified port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
